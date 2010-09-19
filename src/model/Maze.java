444aa8ebfa2da4edb6e8356f4bcb66404692cc2c
Ivar Fredriksen <ivar@iterate.no>
2010-09-19 21:39:17 +0200

    First attempt of a randomization algorithm to create a maze.

--- src/model/Maze.java
+++ src/model/Maze.java
@@ -1,18 +1,23 @@
 package model;
 
+import program.Generator;
+
 public class Maze {
 	
 	private Room[][] rooms;
+	private int dimension;
 	
 	public Maze(int dimension) {
 		rooms = new Room[dimension][dimension];
+		this.dimension = dimension;
 		initRooms();		
-		setEntrance(dimension);
-		setOuterWalls(dimension);
+		setEntrance();
+		setOuterWalls();
+		generateInitialPath(100);
 		
 	}
 
-	private void setOuterWalls(int dimension) {
+	private void setOuterWalls() {
 		for(int row = 0; row < dimension; row++) {
 			for(int column = 0; column < dimension; column++) {
 				if(row == 0 || row == dimension-1 || column == 0 || column == dimension-1) {
@@ -22,7 +27,7 @@ public class Maze {
 		}
 	}
 
-	private void setEntrance(int dimension) {
+	private void setEntrance() {
 		//entrance is set to the room that is to the far south, and in the middle
 		rooms[dimension-1][(dimension-1)/2].setEntrance(true);
 		//room directly north of entrance is set to path
@@ -33,6 +38,50 @@ public class Maze {
 		for (int row = 0; row < rooms.length; row++) {
 			for (int column = 0; column < rooms[row].length; column++) {
 				rooms[row][column] = new Room();
+			}
+		}
+	}
+	
+	private void generateInitialPath(int steps) {
+		//start at room north of entrance
+		int currentRow = dimension-2;
+		int currentColumn = (dimension-1)/2;
+		
+		int nextRow = currentRow;
+		int nextColumn = currentColumn;
+		Room roomToheck;
+		
+		for(int i = 0; i < steps; i++) {
+			int direction = Generator.chooseRandomDirection();
+			// go north, -1 row
+			if(direction == 1){
+				nextRow = currentRow -1;
+				nextColumn = currentColumn;
+			}
+			//go east, +1 column
+			else if(direction == 2){
+				nextRow = currentRow;
+				nextColumn = currentColumn +1;
+				
+			}
+			//go south, row +1;
+			else if(direction == 3){
+				nextRow = currentRow+1;
+				nextColumn = currentColumn;
+			}
+			//go west, -1 column
+			else if(direction == 4){
+				nextRow = currentRow;
+				nextColumn = currentColumn-1;
+			}
+			
+			if(nextColumn <= dimension || nextRow <= dimension) {
+				roomToheck = getRoom(nextRow, nextColumn);
+				if(!roomToheck.isEntrance && !roomToheck.isWall) {
+					roomToheck.setPath(true);
+					currentRow = nextRow;
+					currentColumn = nextColumn;
+				}
 			}
 		}
 	}
