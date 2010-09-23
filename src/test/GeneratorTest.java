b9ef93e813247270dfb3836ef1c066e29d9937fc
Ivar Fredriksen <ivar@iterate.no>
2010-09-23 20:27:08 +0200

    Impoved random tests, to run many times ( to ensure better safty in tests) --- could statistically not catch all cases, but not really worried

--- src/test/GeneratorTest.java
+++ src/test/GeneratorTest.java
@@ -11,12 +11,21 @@ public class GeneratorTest {
 	@Test
 	public void chooseRandomDirectionTest() throws Exception {
 		String direction = Generator.chooseRandomDirection();
-		boolean success = false;
-		if(direction.equals("N")|| direction.equals("E") || direction.equals("S") || direction.equals("W")) {
-			success = true;
+		for(int i = 0; i < 1000; i++) {
+			boolean success = false;
+			if(direction.equals("N")|| direction.equals("E") || direction.equals("S") || direction.equals("W")) {
+				success = true;
+			}
+			assertTrue(success);
 		}
-		assertTrue(success);
 	}
 	
-	
+	@Test
+	public void randomCoordinateIsWithinDimention() throws Exception {
+		for(int i = 0; i < 1000; i++) {
+			int test = Generator.randomCooridnate(10);
+			assertTrue(test < 10);
+			assertTrue(test > 0);
+		}
+	}
 }
